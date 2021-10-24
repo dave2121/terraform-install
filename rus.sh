8bbfe493df0de3f0a1c294b62e68d9c61826ff90
Your Name <you@example.com>
2021-10-24 20:47:18 +0000

    change

--- rus.sh
+++ rus.sh
@@ -6,4 +6,4 @@ sudo wget https://releases.hashicorp.com/terraform/1.0.0/terraform_1.0.0_linux_a
 sudo apt install unzip -y
 sudo unzip terraform_1.0.0_linux_amd64
 sudo mv terraform /usr/bin/
-sudo rm -r /home/admin/terraform-install/terraform_1.0.0_linux_amd64.zip
+sudo rm -r /home/admin/dave/terraform-install/terraform_1.0.0_linux_amd64.zip
